2631cf9978b0c32167480b4bd5071e19e8032480
Heetika Vaity <vaityheetika22@gmail.com>
2024-07-11 02:33:35 +0530

    home page done

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -1,26 +1,46 @@
-import { FlatList, Image, StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { Alert, FlatList, Image, RefreshControl, StyleSheet, Text, View } from 'react-native'
+import React, { useEffect, useState } from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import { StatusBar } from 'expo-status-bar'
 import {images} from '../../constants'
 import SearchInput from '../../components/SearchInput'
+import Trending from '../../components/Trending'
+import EmptyState from '../../components/EmptyState'
+import { getAllPosts, getLatestPosts } from '../../lib/appwrite'
+import useAppwrite from '../../lib/useAppwrite'
+import VideoCard from '../../components/VideoCard'
 
 const Home = () => {
+
+  const {data: posts, refetch} = useAppwrite(getAllPosts);
+  //console.log(posts);
+
+  const {data: latestPosts, } = useAppwrite(getLatestPosts);
+  console.log(latestPosts);
+
+  const[refreshing, setRefreshing]= useState(false);
+  const onRefresh= async () =>{
+    setRefreshing(true);
+    //re call videos if any new videos come
+    await refetch();
+    setRefreshing(false);
+  }
+
   return (
-    <SafeAreaView className="bg-primary">
+    <SafeAreaView className="bg-primary h-full">
       <StatusBar backgroundColor='#161622' style="light"></StatusBar>
       <FlatList 
-        data={[{id:1}]}
+        data={posts}
         keyExtractor={(item)=>{item.$id}}
         renderItem={({item}) => (
-            <Text className="text-3xl text-white">{item.id}</Text>
+            <VideoCard video={item}/>
         )}
         ListHeaderComponent={() => (
           <View className="my-6 px-4 space-y-6">
             <View className="justify-between flex-row items-start mb-6">
               <View>
                 <Text className="font-pmedium text-sm text-gray-100">Welcome Back</Text>
-                <Text className="text-2xl font-psemiblod text-white">Heetika</Text>
+                <Text className="text-2xl font-psemibold text-white">Heetika</Text>
               </View>
 
               <View className="mt-1.5">
@@ -34,10 +54,16 @@ const Home = () => {
               <Text className="text-gary-100 text-white font-pregular">
                 Latest Videos
               </Text>
-              
+              <Trending posts={latestPosts} />
             </View>
           </View>
         )}
+
+        ListEmptyComponent={() => (
+          <EmptyState title="No Videos Found" subtitle="Be the first one to upload a video" />
+        )}
+
+        refreshControl={<RefreshControl refreshing= {refreshing} onRefresh={onRefresh}/>}
       />
     </SafeAreaView>
   )
