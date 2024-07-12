514336097e28dc11441a254fb763e4b865cd2b21
Heetika Vaity <vaityheetika22@gmail.com>
2024-07-12 23:56:58 +0530

    Layout for search videos, hide configuartions

--- app/(tabs)/saved.jsx
+++ app/(tabs)/saved.jsx
@@ -1,14 +1,59 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { Alert, FlatList, Image, RefreshControl, StyleSheet, Text, View } from 'react-native'
+import React, { useEffect, useState } from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import { StatusBar } from 'expo-status-bar'
+import {images} from '../../constants'
+import SearchInput from '../../components/SearchInput'
+import EmptyState from '../../components/EmptyState'
+import { getAllPosts, getLatestPosts } from '../../lib/appwrite'
+import useAppwrite from '../../lib/useAppwrite'
+import VideoCard from '../../components/VideoCard'
+import { useGlobalContext } from '../../context/GlobalProvider'
 
-const Bookmark = () => {
+const Saved = () => {
+  const {user, setUser, setIsLoggedIn} = useGlobalContext();
+
+  const {data: posts, refetch} = useAppwrite(getAllPosts);
+  //console.log(posts);
+
+  const[refreshing, setRefreshing]= useState(false);
+  const onRefresh= async () =>{
+    setRefreshing(true);
+    //re call videos if any new videos come
+    await refetch();
+    setRefreshing(false);
+  }
+
   return (
-    <View>
-      <Text>BookMark</Text>
-    </View>
+    <SafeAreaView className="bg-primary h-full">
+      <StatusBar backgroundColor='#161622' style="light"></StatusBar>
+      <FlatList 
+        data={posts}
+        keyExtractor={(item)=>item.$id}
+        renderItem={({item}) => (
+            <VideoCard video={item}/>
+        )}
+        ListHeaderComponent={() => (
+          <View className="my-6 px-4 space-y-6">
+            <View className="justify-between flex-row items-start mb-6">
+             <Text className="text-2xl text-white font-psemibold">Saved Videos</Text>
+            </View>
+
+            <SearchInput placeholder="Search your saved videos"/>
+
+          </View>
+        )}
+
+        ListEmptyComponent={() => (
+          <EmptyState title="No Saved Videos" subtitle="Save your favourite videos now.." />
+        )}
+
+        refreshControl={<RefreshControl refreshing= {refreshing} onRefresh={onRefresh}/>}
+      />
+    </SafeAreaView>
   )
 }
 
-export default Bookmark
+export default Saved
 
 const styles = StyleSheet.create({})
